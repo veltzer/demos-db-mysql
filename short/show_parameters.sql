88d8e16fcafd0a882bb50d4cc5056016d8b8b1c7
Mark Veltzer <mark.veltzer@gmail.com>
2024-04-29 10:54:23 +0300

    no commit message given

--- short/show_parameters.sql
+++ short/show_parameters.sql
@@ -1,3 +1,4 @@
 #!/usr/bin/env mysql_run.sh
 SELECT @@general_log;
+SELECT @@log_output;
 SHOW VARIABLES LIKE 'general%';
--- short/turn_on_general_log.sql
+++ short/turn_on_general_log.sql
@@ -1,4 +1,4 @@
 #!/usr/bin/env mysql_run.sh
-SET GLOBAL general_log = on;
+SET GLOBAL general_log = ON;
 SET GLOBAL general_log_file="/var/log/mysql/mysql.log";
-SET GLOBAL log_output = "file";
+SET GLOBAL log_output = "FILE";
