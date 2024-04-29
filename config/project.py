e2be9f472ea901adc47d887cc99726e3d68e6f76
Mark Veltzer <mark.veltzer@gmail.com>
2024-04-29 09:29:09 +0300

    no commit message given

---
+++ config/project.py
@@ -0,0 +1,7 @@
+name = "demos-mysql"
+description_short = "Demos for the mysql database server"
+keywords = [
+    "mysql",
+    "SQL",
+    "study",
+]
---
+++ config/python.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+dev_requires: List[str] = [
+    "pypitools",
+    "black",
+]
+config_requires: List[str] = [
+    "pyclassifiers",
+]
+install_requires: List[str] = [
+    "termcolor",
+    "yattag",
+]
+build_requires: List[str] = [
+    "pymakehelper",
+    "pydmt",
+]
+test_requires: List[str] = [
+    "pylint",
+    "pytest",
+    "pytest-cov",
+    "flake8",
+    "pyflakes",
+    "mypy",
+    "pylogconf",
+    "types-termcolor",
+    "types-PyYAML",
+]
+requires = config_requires + install_requires + build_requires + test_requires
