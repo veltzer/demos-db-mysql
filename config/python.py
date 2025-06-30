c5ff1e2bb3f0f7ceb6e7ff26587a0f1bc683ef1d
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-30 13:16:05 +0300

    fixed up deps

--- config/python.py
+++ config/python.py
@@ -1,21 +1,18 @@
 """ python deps for this project """
 
+
+import config.shared
+
+
 install_requires: list[str] = [
     "termcolor",
     "yattag",
     "pylogconf",
 ]
-build_requires: list[str] = [
-    "pydmt",
-    "pymakehelper",
-]
-test_requires: list[str] = [
-    "pylint",
-    "pytest",
-    "mypy",
-    "ruff",
-    # types
+build_requires: list[str] = config.shared.BUILD
+test_requires: list[str] = config.shared.TEST
+types_requires: list[str] = [
     "types-termcolor",
     "types-PyYAML",
 ]
-requires = install_requires + build_requires + test_requires
+requires = install_requires + build_requires + test_requires + types_requires
